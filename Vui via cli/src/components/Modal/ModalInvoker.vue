e2e25914a24865a45932e6ebd4f958f2fcbc603d
Андрей Тимофеев <anke2010@yandex.ru>
2022-09-04 02:47:41 +0300

    added vue cli, pairs game and docker

---
+++ Vui via cli/src/components/Modal/ModalInvoker.vue
@@ -0,0 +1,67 @@
+<template>
+  <h2>Show {{ type }} modal</h2>
+  <input type="text" ref="name" placeholder="Modal title" />
+  <input type="text" ref="value" placeholder="Modal text" />
+  <button @click="togleModal">Show modal window</button>
+
+  <Modal
+    v-show="showModal"
+    :title="modalTitle"
+    :value="modalMessage"
+    :elements="books"
+    :theme="type"
+    @close="togleModal"
+  >
+    <template v-slot:actions>
+      <a href="http://placeholder.pro">My site</a>
+    </template>
+    <template v-slot:header>
+      <h2>(slot){{ modalTitle }}</h2>
+      <p>{{ modalMessage }}</p>
+    </template>
+  </Modal>
+</template>
+
+<script>
+import Modal from "./Modal.vue";
+
+export default {
+  components: {
+    Modal,
+  },
+  props: ["type"],
+  data() {
+    return {
+      title: "My app",
+      modalTitle: "New Message",
+      modalMessage: "Message is hear",
+      books: [
+        { title: "kek", value: 1 },
+        { title: "jep", value: 2 },
+        { title: "zep", value: 3 },
+        { title: "lep", value: 4 },
+        { title: "hep", value: 5 },
+      ],
+      showModal: false,
+    };
+  },
+  methods: {
+    togleModal() {
+      this.modalTitle = this.$refs.name.value;
+      this.modalMessage = this.$refs.value.value;
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
+<style>
+.inputs {
+  margin: 10px;
+}
+
+input,
+button {
+  margin: 8px;
+}
+</style>
---
+++ Vui via cli/src/components/PairGame/GameBoard.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="container">
+    <div class="wrapper">
+      <GameCard
+        ref="cards"
+        v-for="(item, index) in gameCards"
+        :key="index"
+        :cardName="item"
+        :cardIndex="index"
+        :board="this"
+        @cardSelected="selectCard"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import GameCard from "./GameCarrd.vue";
+
+export default {
+  components: {
+    GameCard,
+  },
+  emits: ["clickIsMade", "addScore", "getTotal"],
+  data() {
+    return {
+      selectedCard: null,
+      isWaiting: false,
+      gameCards: [],
+    };
+  },
+  methods: {
+    async selectCard(item) {
+      this.$emit("clickIsMade");
+      if (this.selectedCard === null) {
+        this.selectedCard = item;
+      } else if (this.selectedCard.cardName === item.cardName) {
+        this.$emit("addScore");
+        this.selectedCard = null;
+      } else {
+        this.isWaiting = true;
+        await sleep(1000);
+        this.isWaiting = false;
+
+        this.selectedCard.hideBox();
+        item.hideBox();
+
+        this.selectedCard = null;
+      }
+    },
+
+    getCards(dificulty) {
+      if (this.$refs["cards"] !== undefined)
+        this.$refs["cards"].forEach((q) => {
+          q.hideBox();
+        });
+      let cardsCount = 0;
+      switch (dificulty) {
+        case 1:
+          cardsCount = 6;
+          break;
+        case 2:
+          cardsCount = 8;
+          break;
+        case 3:
+          cardsCount = 10;
+          break;
+        default:
+          cardsCount = 12;
+          break;
+      }
+      this.$emit("getTotal", cardsCount);
+
+      let selectedCards = [];
+      while (selectedCards.length < cardsCount * 2) {
+        let selection = names[randomIntFromInterval(0, names.length - 1)];
+        if (!selectedCards.includes(selection)) {
+          selectedCards.push(selection);
+          selectedCards.push(selection);
+        }
+      }
+      shuffle(selectedCards);
+      this.gameCards = selectedCards;
+    },
+  },
+};
+
+let names = [
+  "🍎",
+  "🍐",
+  "🛴",
+  "🚙",
+  "🌄",
+  "🌅",
+  "🏡",
+  "🏭",
+  "💾",
+  "😀",
+  "🐱",
+  "🐶",
+  "🌳",
+  "🌼",
+  "📏",
+  "📃",
+];
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: inline-grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-gap: 10px;
+  color: #444;
+}
+</style>
